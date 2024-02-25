1d067bf103b32877b92459341ee9083a092ce8f6
Gabor Kukucska <gabor@buyahmedia.com>
2024-02-25 23:09:17 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from petals.constants import PUBLIC_INITIAL_PEERS
 
 from data_structures import ModelInfo
 
-INITIAL_PEERS = /ip4/49.194.161.134/tcp/31337/p2p/QmNbWmdMF4mrHYBZvaX1aWmEHw1i6Sh7E1sVZdqE1LFbFm
+INITIAL_PEERS = /ip4/49.194.161.134/tcp/31337/p2p/QmXWbuJQTs1eQiwV6SpPdup4hXXvKmNZCpX5XrRYk3EgVj
 
 MODELS = [
     ModelInfo(
@@ -11,19 +11,8 @@ MODELS = [
         num_blocks=80,
     ),
     ModelInfo(
-        dht_prefix="falcon-180B-chat",
-        repository="https://huggingface.co/tiiuae/falcon-180B-chat",
-        num_blocks=80,
-        limited=True,
-    ),
-    ModelInfo(
         dht_prefix="CodeLlama-34b-Instruct-hf",
         repository="https://huggingface.co/codellama/CodeLlama-34b-Instruct-hf",
-        num_blocks=80,
-    ),
-    ModelInfo(
-        dht_prefix="Llama-2-70b-hf",
-        repository="https://huggingface.co/meta-llama/Llama-2-70b-hf",
         num_blocks=80,
     ),
 ]
